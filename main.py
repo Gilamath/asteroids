73a96e15b6bca0587c410a98b48825e3761914a6
Talha <rao.talha@icloud.com>
2025-05-24 06:50:57 -0500

    set up black screen (works properly on Mac now)

--- main.py
+++ main.py
@@ -9,9 +9,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        for event in pygame.event.get():    # event loop; makes window's close button work
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()   # display update; call last
 
 if __name__ == "__main__":
     main()
