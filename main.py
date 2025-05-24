36d856481df8d4765993460de87acee234f4cf7d
Talha <rao.talha@icloud.com>
2025-05-24 07:02:03 -0500

    set fps to 60

--- main.py
+++ main.py
@@ -14,12 +14,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    fps_clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():    # event loop; makes window's close button work
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()   # display update; call last
+        dt = fps_clock.tick(60) / 1000  # 60 fps
 
 if __name__ == "__main__":
     main()
