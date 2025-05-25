91482e90e758e5c33d6339b632a78867df11cb65
Talha <rao.talha@icloud.com>
2025-05-24 21:07:16 -0500

    built out a Bullet class

--- bullets.py
+++ bullets.py
@@ -6,3 +6,13 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+
+class Bullet(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
+
+    def update(self, delta_time):
+        self.position += self.velocity * delta_time
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius)
